61664dc77d94411439c584851efb361b3edb08fd
Zabihamdam <45077054+Zabihamdam@users.noreply.github.com>
2019-02-01 22:19:56 -0500

    Update LogInTests.java
    added logInTest2 method again

--- src/test/java/Tests/LogInTests.java
+++ src/test/java/Tests/LogInTests.java
@@ -40,7 +40,15 @@ public class LogInTests {
         driver.findElement(By.id("ctl00_logout")).click();
         Assert.assertEquals(driver.getTitle(),"Web Orders Login" );
     }
+  @Test
+    public void logInTest2(){
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
 
+        Assert.assertEquals(driver.getTitle(),"Web Orders");
+
+    }
     @AfterMethod
     public void logOutTest(){
         driver.close();
